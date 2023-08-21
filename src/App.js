a041d95c4219ccceab4fbfa94f7bb63243415f1a
Mahdi Pasandideh <mtipsd.git@gmail.com>
2023-08-21 16:51:09 +0330

    handle previous and next steps using useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
+const messages = [
+  'Learn React',
+  'Apply for Jobs',
+  'Invest your new income',
+];
+
 export default function App() {
+  const step = 1;
+
+  function handlePrevious() {
+    console.log('Previous Button Clicked');
+  }
+
+  function handleNext() {
+    console.log('Next Button Clicked');
+  }
+
+  /////////////////////////////////////////
   return (
     <div className='steps'>
       <div className='numbers'>
-        <div className='1'></div>
-        <div className='2'></div>
-        <div className='3'></div>
+        <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
+        <div className={`${step >= 2 ? 'active' : ''}`}> 2</div>
+        <div className={`${step >= 3 ? 'active' : ''}`}> 3</div>
       </div>
 
-      <div className='message'>hello</div>
+      <div className='message'>
+        Step {step}: {messages[step - 1]}
+      </div>
 
       <div className='buttons'>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: '#7950f2', color: '#fff' }}>
+        <button
+          style={{ backgroundColor: '#7950f2', color: '#fff' }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
